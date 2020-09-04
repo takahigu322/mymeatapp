09abe0fd277ad697f0e50f57bb7794075f315ade
takahigu322 <takahigu642@gmail.com>
2020-09-04 23:42:20 +0900

    style

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -13,6 +13,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text("DeliMeal"),
       ),
       body: GridView(
+        padding: EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map((catData) => CategoryItem(catData.title, catData.color))
             .toList(),
