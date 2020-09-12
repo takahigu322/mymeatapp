c92a529b05a8b8afd0d8e51f1fdd9f0e2a0023f2
takahigu322 <takahigu642@gmail.com>
2020-09-12 23:19:20 +0900

    update category_page

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("The Recipes"),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text("The Recipes"),
